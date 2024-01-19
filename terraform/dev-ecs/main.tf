e1a7e173814d37c2d774696eed3d3d3ad03e17b8
Emmanuel <emmanuelra01@gmail.com>
2024-01-19 03:33:33 -0600

    Corregimos ecs main terraform

--- terraform/dev-ecs/main.tf
+++ terraform/dev-ecs/main.tf
@@ -34,7 +34,7 @@ resource "aws_ecs_task_definition" "task" {
 }
 
 resource "aws_ecs_service" "service" {
-  name             = "service"
+  name             = "test-magiclog-dev"
   cluster          = aws_ecs_cluster.cluster.id
   task_definition  = aws_ecs_task_definition.task.id
   desired_count    = 1
